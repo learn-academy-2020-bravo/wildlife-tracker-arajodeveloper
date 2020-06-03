cb2ab10b5584be32020044240a0b55cabfa0dd12
Ara Jo <arajodeveloper@gmail.com>
2020-06-03 14:02:43 -0700

    two databases merge

--- app/controllers/SightingsController.rb
+++ app/controllers/SightingsController.rb
@@ -9,6 +9,25 @@ class SightingsController < ApplicationController
         sighting = Sighting.find(params[:id])
         render json: sighting
       end
+
+      def destroy
+        sighting = Sighting.find(params[:id])
+        if sighting.destroy
+          render json: sighting
+        else
+          render json: sighting.errors
+        end
+      end
+    
+      def update
+        sighting = Sighting.find(params[:id])
+        sighting.update(sighting_params)
+        if sighting.valid?
+          render json: sighting
+        else
+          render json: sighting.errors
+        end
+      end
     
     def create
         sighting = Sighting.create (sighting_params)
